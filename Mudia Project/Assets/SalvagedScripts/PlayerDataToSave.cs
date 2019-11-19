cf9f93a613fe9e2cf73e5c0b03adfa7e3befffd3
AsylumRunner479 <54158974+AsylumRunner479@users.noreply.github.com>
2019-11-20 09:13:42 +1100

    added save scripts

---
+++ Mudia Project/Assets/SalvagedScripts/PlayerDataToSave.cs
@@ -0,0 +1,67 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+[System.Serializable]
+public class PlayerDataToSave
+{
+    //Data....Get from Game
+    public string playerName;
+    public int level;
+    public string checkPoint;
+    public float maxHealth, maxMana, maxStamina;
+    public float curHealth, curMana, curStamina;
+    public float posx, posy, posz;
+    public float rotx, roty, rotz, rotw;
+
+    public static int saveSlot;
+    public int[] stats = new int[6];
+    public int classIndex;
+    public int raceIndex;
+    public int skinIndex, eyesIndex, mouthIndex, hairIndex, clothesIndex, armourIndex;
+    public PlayerDataToSave(PlayerHandler player)
+    {
+        playerName = player.characterName;
+        level = 0;
+        if (player.curCheckPoint != null)
+        {
+            checkPoint = player.curCheckPoint.name;
+            posx = player.transform.position.x;
+            posy = player.transform.position.y;
+            posz = player.transform.position.z;
+
+            rotx = player.transform.rotation.x;
+            roty = player.transform.rotation.y;
+            rotz = player.transform.rotation.z;
+            rotw = player.transform.rotation.w;
+        }
+        else
+        {
+            checkPoint = player.firstCheckPointName;
+            posx = 0;
+            posy = 0;
+            posz = 0;
+        }
+        maxHealth = player.maxHealth;
+        maxMana = player.maxMana;
+        maxStamina = player.maxStamina;
+
+        curHealth = player.curHealth;
+        curMana = player.curMana;
+        curStamina = PlayerHandler.curStamina;
+
+        for (int i = 0; i < 6; i++)
+        {
+            stats[i] = player.stats[i].value;
+        }
+
+        skinIndex = player.skinIndex;
+        hairIndex = player.hairIndex;
+        mouthIndex = player.mouthIndex;
+        eyesIndex = player.eyesIndex;
+        clothesIndex = player.clothesIndex;
+        armourIndex = player.armourIndex;
+        raceIndex = (int)player.characterRace;
+        classIndex = (int)player.characterClass;
+    }
+
+}
