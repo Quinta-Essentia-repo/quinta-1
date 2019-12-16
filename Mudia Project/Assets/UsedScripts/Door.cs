039bf739441b3096bcee8f7a5c1e0f24ec66cea4
AsylumRunner479 <54158974+AsylumRunner479@users.noreply.github.com>
2019-12-17 10:19:05 +1100

    built in sliding doors

---
+++ Mudia Project/Assets/UsedScripts/Door.cs
@@ -0,0 +1,21 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Door : MonoBehaviour
+{
+    [SerializeField]
+    GameObject door1,door2;
+    
+
+    private void OnTriggerEnter(Collider player)
+    {
+        door1.transform.position += new Vector3(3, 0, 0);
+        door2.transform.position -= new Vector3(3, 0, 0);
+    }
+    private void OnTriggerExit(Collider player)
+    {
+        door2.transform.position += new Vector3(3, 0, 0);
+        door1.transform.position -= new Vector3(3, 0, 0);
+    }
+}
