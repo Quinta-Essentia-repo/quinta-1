dd9e0c129642b787fe61626e77b157594be92480
AsylumRunner479 <54158974+AsylumRunner479@users.noreply.github.com>
2019-12-26 11:22:42 +1100

    fixed the sliding door bug

--- Mudia Project/Assets/UsedScripts/Door.cs
+++ Mudia Project/Assets/UsedScripts/Door.cs
@@ -6,16 +6,34 @@ public class Door : MonoBehaviour
 {
     [SerializeField]
     GameObject door1,door2;
+    public bool vertical;
     
 
     private void OnTriggerEnter(Collider player)
     {
-        door1.transform.position += new Vector3(3, 0, 0);
-        door2.transform.position -= new Vector3(3, 0, 0);
+        if (vertical)
+        {
+            door1.transform.position += new Vector3(0, 0, 3);
+            door2.transform.position -= new Vector3(0, 0, 3);
+        }
+        else
+        {
+            door1.transform.position += new Vector3(3, 0, 0);
+            door2.transform.position -= new Vector3(3, 0, 0);
+        }
     }
     private void OnTriggerExit(Collider player)
     {
-        door2.transform.position += new Vector3(3, 0, 0);
-        door1.transform.position -= new Vector3(3, 0, 0);
+
+        if (vertical)
+        {
+            door2.transform.position += new Vector3(0, 0, 3);
+            door1.transform.position -= new Vector3(0, 0, 3);
+        }
+        else
+        {
+            door2.transform.position += new Vector3(3, 0, 0);
+            door1.transform.position -= new Vector3(3, 0, 0);
+        }
     }
 }
